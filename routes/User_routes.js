fdb20e6d8fbbc1ee9c98ef3694d4c99c73642f98
Ryan CHANG-HA LEE <13410@coderacademy.edu.au>
2023-02-03 01:53:22 +1000

    JWT built

--- routes/User_routes.js
+++ routes/User_routes.js
@@ -1,31 +1,20 @@
 import express from 'express'
 import { UserModel } from "../db.js"
-
+import jwt from 'jsonwebtoken'
+import { verifyToken } from '../middleware/auth.js';
 
 const router = express.Router();
+const SECRET_KEY = process.env.SECRET_KEY;
 
+// All users list
 router.get('/', async (req, res) => {
   console.log("Access to find all users");
+  // res.send(await UserModel.find().populate({ path: 'users', select: 'name' }))});
   res.send(await UserModel.find())});
 
-
-router.get('/:id', async (req, res) => {
-  // console.log("Access to find a user")
-    try {
-      const user = await UserModel.findById(req.params.id)
-      if (user) {
-        res.send(user)
-      } else {
-        res.status(404).send({ error: 'User not found' })
-      }
-    }
-    catch (err) {
-      res.status(500).send({ error: err.message })
-    }
-})
-
-router.post('/', async (req, res) => {
-  console.log("Access for creating a user")
+// Register user
+router.post('/register', async (req, res) => {
+  console.log("Access to register a user")
   try {
     // 1. Create a new user object with values passed in from the request
     const { email, title, firstName, lastName, phoneNumber, password } = req.body
@@ -42,20 +31,60 @@ router.post('/', async (req, res) => {
     // 2. Insert the new user into the database
     const insertedUser = await UserModel.create(newUser)
     // 3. Send the new user with 201 status
-    res.status(201).send(insertedUser)
+    const token = jwt.sign({
+      type: 'JWT',
+      email: req.body.email,
+      firstName: req.body.firstName
+    }, SECRET_KEY, {
+      expiresIn: '30m',
+      issuer: 'PAWFUL_Dev'
+    })
+    return res.status(201).json({
+      code: 201,
+      message: 'Thanks for registering!',
+      user_id: insertedUser._id,
+      firstName: insertedUser.firstName,
+      token: token
+    })
   } catch (err) {
     res.status(500).send({ error: err.message })
   }
 })
-  
-  // Update
-router.put('/:id', async (req, res) => {
-    const { email, password, title, firstName, lastName, phoneNumber, isAdmin } = req.body
-    const updatedUser = { $set:{email}, $set:{password}, title, firstName, lastName, phoneNumber, $set:{isAdmin} }
-    
+
+// For login purposes
+router.post('/login', async (req, res) => {
+  console.log("Access to find all users");
+  try {
+    const user = await UserModel.findOne(req.body)
+    if (user.password === req.body.password) {
+      const token = jwt.sign({
+        type: 'JWT',
+        email: req.body.email,
+        firstName: req.body.firstName
+      }, SECRET_KEY, {
+        expiresIn: '30m',
+        issuer: 'PAWFUL_Dev'
+      })
+      return res.status(200).json({
+        code: 201,
+        message: 'Welcome back to PAWFUL!',
+        user_id: user._id,
+        firstName: user.firstName,
+        token: token
+      })
+    } else {
+      return res.status(404).send({ error: err.message })
+    }
+  } catch (err) {
+    res.status(500).send({ error: err.message })
+  }
+});
+
+// Get user details before updating my details 
+router.get('/:id', verifyToken, async (req, res) => {
+  // console.log("Access to find a user")
     try {
-      console.log("Updating user requested")
-      const user = await UserModel.findByIdAndUpdate(req.params.id, updatedUser, { returnDocument: 'after'})
+      const user = await UserModel.findById(req.params.id)
       if (user) {
         res.send(user)
       } else {
@@ -67,8 +96,38 @@ router.put('/:id', async (req, res) => {
     }
 })
 
+
+  
+// Update user's detail
+router.put('/:id', verifyToken, async (req, res) => {
+    const { email, password, title, firstName, lastName, phoneNumber, isAdmin } = req.body
+    const updatedUser = { 
+      $set:{email}, 
+      $set:{password}, 
+      title, 
+      firstName, 
+      lastName, 
+      phoneNumber, 
+      $set:{isAdmin} }
+    try {
+      console.log("Updating user requested")
+      const user = await UserModel.findByIdAndUpdate(req.params.id, updatedUser, { returnDocument: 'after'})
+      if (user) {
+        res.status(201).json({
+          code: 201,
+          message: 'Successfully saved!'
+        })
+      } else {
+        res.status(404).send({ error: 'Woof! User is not found!' })
+      }
+    }
+    catch (err) {
+      res.status(500).send({ error: 'Failed to save the data! Please try again later.' })
+    }
+})
+
   // Delete
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', verifyToken, async (req, res) => {
     try {
       const user = await UserModel.findByIdAndDelete(req.params.id)
       if (user) {
