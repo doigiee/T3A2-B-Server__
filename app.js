8bc57f477e6afa49e865d2d55885f8767eff1621
doigiee <doigiee@live.com>
2023-02-02 18:20:08 +1000

    updated

--- app.js
+++ app.js
@@ -10,10 +10,6 @@ app.use(cors())
 
 app.use(express.json())
 
-
-app.use('/users', userRoutes)
-app.use('/bookings', bookingRoutes)
-
 // Home page 
 app.get('/', (req, res) => {
   res.send('Home Route');
